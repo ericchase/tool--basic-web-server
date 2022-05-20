d3ed71bddb9ca0d9e5274513dbd50bb8b8a8998b
ericchase <>
2022-05-20 04:38:21 -0500

    feat(powershell): run local deno executable

--- server.ps1
+++ server.ps1
@@ -1,5 +1,13 @@
-while ($true) { 
-    deno run --allow-env=PORT --allow-net --allow-read server.ts
+$local_executable = (Test-Path -Path .\deno.exe -PathType Leaf)
+
+while ($true) {
+    if ($local_executable) {
+        .\deno run --allow-env=PORT --allow-net --allow-read server.ts
+    }
+    else {
+        deno run --allow-env=PORT --allow-net --allow-read server.ts
+    }
+
     if ($LASTEXITCODE -eq 0) { 
         $response = Read-Host "Restart? (y/n)"
         if ($response -ne "y")
